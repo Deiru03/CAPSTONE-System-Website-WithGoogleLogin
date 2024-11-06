26d6c1a4a43a0c0da52ad33dea7daac96e63fbd5
Deiru <deiru03@gmail.com>
2024-11-06 21:31:09 +0800

    clearance view page fix

--- resources/views/faculty/views/clearances.blade.php
+++ resources/views/faculty/views/clearances.blade.php
@@ -20,6 +20,23 @@
                                 <p class="text-sm text-green-600">Here you can view and manage your clearances efficiently.</p>
                             </div>
                         </div>
+                        <div class="bg-white p-2">
+                            <a href="{{ route('faculty.generateClearanceReport') }}" class="inline-flex items-center px-4 py-2 {{ $userInfo->clearances_status === 'complete' ? 'bg-green-500 hover:bg-green-600' : 'bg-yellow-500 hover:bg-yellow-600' }} text-white text-sm font-medium rounded-md transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110">
+                                <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"></path>
+                                </svg>
+                                Generate Checklist Pass
+                            </a>
+                            <span class="mx-2"></span>
+                            <a href="{{ route('faculty.clearances.index') }}" class="inline-flex items-center px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white text-sm font-medium rounded-md transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110">
+                                <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"></path>
+                                </svg>
+                                View Clearance Checklists
+                            </a>
+                        </div>
+                        {{-- TESTING --}}
+                        {{-- @include('faculty.views.clearances.clearance-show', ['userClearance' => $userClearance, 'isInclude' => true]) --}}
                     @else
                         <div class="flex items-center mb-6 bg-blue-100 p-4 rounded-lg shadow-md">
                             <svg class="w-8 h-8 text-blue-500 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -30,30 +47,29 @@
                                 <p class="text-sm text-blue-600">To get started, please obtain a copy of your clearance from the shared clearances list.</p>
                             </div>
                         </div>
-                       
-                    @endif
-                    @if($userClearance)
-                    <div class="bg-white p-2">
-                        <a href="{{ route('faculty.generateClearanceReport') }}" class="inline-flex items-center px-4 py-2 {{ $userInfo->clearances_status === 'complete' ? 'bg-green-500 hover:bg-green-600' : 'bg-yellow-500 hover:bg-yellow-600' }} text-white text-sm font-medium rounded-md transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110">
-                            <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"></path>
-                            </svg>
-                            Generate Checklist Pass
-                        </a>
-                        <span class="mx-2"></span>
-                        <a href="{{ route('faculty.clearances.index') }}" class="inline-flex items-center px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white text-sm font-medium rounded-md transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110">
-                            <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"></path>
-                            </svg>
-                            View Clearance Checklists
-                        </a>
-                    </div>
                     @endif
                 </div>
             </div>            
         </div>
     </div>
-
-    <!-- Include the clearance-show view -->
-    @include('faculty.views.clearances.clearance-show', ['userClearance' => $userClearance, 'isInclude' => true])
+    @if($userClearance)
+        @include('faculty.views.clearances.clearance-show', ['userClearance' => $userClearance, 'isInclude' => true])
+    @else
+        <div class="mb-6">
+            <div class="container mx-auto px-8 py-12">
+                <div class="text-center">
+                    <i class="fas fa-file-alt text-6xl text-indigo-500 mb-6"></i>
+                    <h2 class="text-4xl font-bold mb-4 text-indigo-800">
+                        No Clearances Available
+                    </h2>
+                    <p class="text-xl text-gray-700 mb-8">
+                        It looks like you haven't obtained a copy of your clearance yet.
+                    </p>
+                    <a href="{{ route('faculty.clearances.index') }}" class="bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-3 px-6 rounded-full transition duration-300 ease-in-out transform hover:scale-105">
+                        Get Your Clearance
+                    </a>
+                </div>
+            </div>
+        </div>
+    @endif
 </x-app-layout>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -115,12 +115,12 @@
                     </a>
 
                     <!-- Test Page -->
-                    <a href="{{ route('faculty.views.test') }}" class="flex items-center px-10 py-4 hover:bg-gray-700 {{ request()->routeIs('faculty.views.test') ? 'bg-gray-700 border-l-4 border-indigo-500' : '' }}">
+                    {{-- <a href="{{ route('faculty.views.test') }}" class="flex items-center px-10 py-4 hover:bg-gray-700 {{ request()->routeIs('faculty.views.test') ? 'bg-gray-700 border-l-4 border-indigo-500' : '' }}">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6 mr-2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 3.104v5.714a2.25 2.25 0 01-.659 1.591L5 14.5M9.75 3.104c-.251.023-.501.05-.75.082m.75-.082a24.301 24.301 0 014.5 0m0 0v5.714c0 .597.237 1.17.659 1.591L19.8 15.3M14.25 3.104c.251.023.501.05.75.082M19.8 15.3l-1.57.393A9.065 9.065 0 0112 15a9.065 9.065 0 00-6.23-.693L5 14.5m14.8.8l1.402 1.402c1.232 1.232.65 3.318-1.067 3.611A48.309 48.309 0 0112 21c-2.773 0-5.491-.235-8.135-.687-1.718-.293-2.3-2.379-1.067-3.61L5 14.5" />
                         </svg>
                         <span class="{{ request()->routeIs('faculty.views.test') ? 'text-indigo-300 font-semibold' : '' }}">Test Page</span>
-                        </a>
+                    </a> --}}
 
                     <!-- Profile -->
                     <a href="{{ route('profile.edit') }}" class="flex items-center px-10 py-4 hover:bg-gray-700 {{ request()->routeIs('profile.edit') ? 'bg-gray-700 border-l-4 border-indigo-500' : '' }}">
@@ -214,17 +214,17 @@
                     <div class="container mx-auto px-4 sm:px-6 lg:px-8 py-8">
                         {{ $slot }} <!-- This is where the content will be injected -->
                     </div>
-                    <footer class="bg-white py-6 mt-auto">
-                        <div class="container mx-auto px-4 text-center text-gray-800">
-                            <p class="text-sm">&copy; 2024 OMSCS IQA ClearVault.</p>
-                            <div class="flex flex-wrap justify-center gap-4 mt-4">
-                                <a href="#" class="hover:text-indigo-400">Privacy Policy</a>
-                                <a href="#" class="hover:text-indigo-400">Terms of Service</a>
-                                <a href="#" class="hover:text-indigo-400">Contact Us</a>
-                            </div>
-                        </div>
-                    </footer>
                 </main>
+                <footer class="bg-white py-6 mt-auto">
+                    <div class="container mx-auto px-4 text-center text-gray-800">
+                        <p class="text-sm">&copy; 2024 OMSCS IQA ClearVault.</p>
+                        <div class="flex flex-wrap justify-center gap-4 mt-4">
+                            <a href="#" class="hover:text-indigo-400">Privacy Policy</a>
+                            <a href="#" class="hover:text-indigo-400">Terms of Service</a>
+                            <a href="#" class="hover:text-indigo-400">Contact Us</a>
+                        </div>
+                    </div>
+                </footer>
             </div>
         </div>
         
