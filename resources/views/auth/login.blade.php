f8a09d99542b05e61179835a884d0129258f50a0
Deiru <deiru03@gmail.com>
2024-12-18 21:50:33 +0800

    fix password login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -25,16 +25,16 @@
         <div class="mb-4 relative">
             <x-input-label for="password" :value="__('Password')" class="text-sm font-medium text-gray-700" />
             <div class="relative">
-                <x-text-input id="password" class="mt-1 block w-full pr-10"
+                <x-text-input id="password" class="mt-1 block w-full pr-4"
                                 type="password"
                                 name="password"
                                 required autocomplete="current-password" />
-                <button type="button" class="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5" onclick="togglePasswordVisibility('password')">
+                {{-- <button type="button" class="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5" onclick="togglePasswordVisibility('password')">
                     <svg class="h-5 w-5 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
                     </svg>
-                </button>
+                </button> --}}
             </div>
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
