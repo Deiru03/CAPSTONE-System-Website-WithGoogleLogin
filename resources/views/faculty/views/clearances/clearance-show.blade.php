1662e62dc3643614025072f2d475fd66873fb849
Deiru <deiru03@gmail.com>
2024-12-19 16:56:11 +0800

    add border blue

--- resources/views/faculty/views/clearances/clearance-show.blade.php
+++ resources/views/faculty/views/clearances/clearance-show.blade.php
@@ -238,7 +238,7 @@
         </div>
 
 
-        <div class="mb-4 p-4 bg-blue-50 rounded-lg border-l-4 border-blue-500 shadow-sm">
+        <div class="mb-4 p-4 bg-blue-50 rounded-lg border-l-4 border-blue-500 shadow-sm border border-blue-200">
             <div class="flex items-center">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-blue-500 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
